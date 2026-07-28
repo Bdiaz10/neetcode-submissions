7c590eee3563f7cc1c60c80ae7e5be7d7c8a2aa6
Braxton Diaz <90151890+Bdiaz10@users.noreply.github.com>
2026-07-28 10:30:17 -0700

    Add: word-ladder - submission-6

---
+++ Data Structures & Algorithms/word-ladder/submission-6.py
@@ -0,0 +1,29 @@
+from collections import deque
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+
+
+        def canTransform(begin, end):
+            invalid = 0
+            for i in range(len(begin)):
+                if begin[i] != end[i]:
+                    invalid += 1
+            return invalid == 1
+
+        q = deque([beginWord])
+        visited = set()
+        visited.add(beginWord)
+        transformations = 1
+        while q:
+            for i in range(len(q)):
+                word = q.popleft()
+                if word == endWord:
+                    return transformations
+                for w in wordList:
+                    if w in visited:
+                        continue
+                    if canTransform(word, w):
+                        q.append(w)
+                        visited.add(w)
+            transformations += 1
+        return 0
