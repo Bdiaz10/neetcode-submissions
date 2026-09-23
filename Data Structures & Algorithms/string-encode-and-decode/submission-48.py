fe46913229dc6d846220ac3b081b77d0a298ea14
Braxton Diaz <90151890+Bdiaz10@users.noreply.github.com>
2026-09-23 11:38:25 -0700

    Add: string-encode-and-decode - submission-48

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-48.py
@@ -0,0 +1,31 @@
+class Solution:
+    # prepend each word with len(word)&
+    # the '&' tells us we reached the end of out prepended text
+
+    # start from idx 0, process the word, move len(word)
+    symbol: str
+
+    def __init__(self):
+        self.symbol = '&'
+
+    def encode(self, strs: List[str]) -> str:
+        result = []
+        for word in strs:
+            result.append(f'{len(word)}{self.symbol}{word}')
+        r = ''.join(result)
+        return r
+
+
+    def decode(self, s: str) -> List[str]:
+        result = []
+        left = 0
+        right = 0
+        while right < len(s):
+            if s[right] == self.symbol:
+                wordLength = int(s[left:right])
+                result.append(s[right+1:right+wordLength+1])
+                right = right+wordLength+1
+                left = right
+            else:
+                right += 1
+        return result
